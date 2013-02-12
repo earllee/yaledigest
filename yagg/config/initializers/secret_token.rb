8a1306204adf685b9a31eb75c63b43f455012ab7
Shenil Dodhia <shenil.dodhia@yale.edu>
2013-02-11 21:56:56 -0500

    Set up rails

---
+++ yagg/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yagg::Application.config.secret_token = 'd497db34c4d164f35a38608575b67184459f2793be58253414783012ebf582b4566c085d186d09ad0e63518f1f92d965744fbb1b28b5fa68fca6044b75bb252d'
