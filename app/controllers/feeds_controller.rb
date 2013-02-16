8b44028076197510f7927531365374d8b463edd6
Shenil Dodhia <shenil.dodhia@yale.edu>
2013-02-16 02:03:50 -0500

    asdf

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -7,7 +7,7 @@ class FeedsController < ApplicationController
 	conn = PGconn.open("dbname=d5do5eebiebbrt host=ec2-107-22-169-108.compute-1.amazonaws.com user=irgffdctcevjds password=akxBZ5CEfIbGgytAF2L9YfQo_E port=5432 sslmode=require")
 	res = conn.exec('SELECT title FROM articles')
 	@titles = res.column_values(0)
-	
+	@articles = res.result
   end
   
   def test
