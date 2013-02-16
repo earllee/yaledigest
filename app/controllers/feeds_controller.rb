51fbe876fb6e8d16164370075ed2bc335053531a
Shenil Dodhia <shenil.dodhia@yale.edu>
2013-02-16 00:56:46 -0500

    feeds work

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,11 @@
+require 'rss/2.0'
+require 'open-uri'
+require 'pg'
+
 class FeedsController < ApplicationController
   def home
+	conn = PGconn.open("dbname=d5do5eebiebbrt host=ec2-107-22-169-108.compute-1.amazonaws.com user=irgffdctcevjds password=akxBZ5CEfIbGgytAF2L9YfQo_E port=5432 sslmode=require")
+	res = conn.exec('SELECT title FROM articles')
+	@titles = res.column_values(0)
   end
 end
