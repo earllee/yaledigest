c58dc2f47a32d2525b436f5bbf556e65124cfaed
Shenil Dodhia <shenil.dodhia@yale.edu>
2013-02-22 12:05:41 -0500

    switching databases

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -5,7 +5,8 @@ require 'pg'
 class FeedsController < ApplicationController
   def home
 	conn = PGconn.open("dbname=d5do5eebiebbrt host=ec2-107-22-169-108.compute-1.amazonaws.com user=irgffdctcevjds password=akxBZ5CEfIbGgytAF2L9YfQo_E port=5432 sslmode=require")
-	res = conn.exec('SELECT * FROM rss_articles')
+	res = conn.exec('select * from (select * from rss_articles2 as orig order by pubDate desc limit 45) as neww order by randid asc;')
+	#	res = conn.exec('SELECT * FROM rss_articles')
 	@titles = res.column_values(0)
 	@articles = res
   end
---
+++ getRSS/testquery.rb
@@ -0,0 +1,25 @@
+require 'open-uri'
+require 'pg'
+require 'nokogiri'
+require 'action_controller'
+
+#include ActionController::Base.helpers
+#params[:text] = "[foo-bar]"
+#puts Regexp.new(Regexp.escape(params[:text]))
+
+conn = PGconn.open("dbname=d5do5eebiebbrt host=ec2-107-22-169-108.compute-1.amazonaws.com user=irgffdctcevjds password=akxBZ5CEfIbGgytAF2L9YfQo_E port=5432 sslmode=require")
+
+#res = conn.exec('SELECT title FROM articles')
+prng = Random.new(524536)
+
+for i in 600..700
+	randid = prng.rand.to_s
+	curid = i.to_s
+	res = conn.exec("UPDATE rss_articles2 SET randID=#{randid} where id=#{curid};")
+end
+
+#lll = res.column_values(0).shuffle
+
+#lll.each do |a|
+#	puts a
+#end
