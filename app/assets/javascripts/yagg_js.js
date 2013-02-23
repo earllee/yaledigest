e33352419fa3937ff5d98cf88607974b6c3c8c71
earllee <earlvlee@gmail.com>
2013-02-23 14:20:52 -0500

    fixing link scroll

--- app/assets/javascripts/yagg_js.js
+++ app/assets/javascripts/yagg_js.js
@@ -2,8 +2,5 @@ $(function(){
 	$('.noscroll').click(function(event) {
 		event.preventDefault();
 	});
-	$('.noscroll').on('click', function(e) {e.preventDefault(); return true;});
+	$("[id$=link]").on('click', function(e) {e.preventDefault(); return true;});
 });
-
-
-$('.noscroll').on('click', function(e) {e.preventDefault(); console.log('This was in yagg_js'); return true;});   
