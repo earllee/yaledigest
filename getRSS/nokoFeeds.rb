af3f2eae3f4097efacca53bf248f0022955e0053
Shenil Dodhia <shenil.dodhia@yale.edu>
2013-02-27 16:46:31 -0500

    updated scraper and icons/logos for publications

--- getRSS/nokoFeeds.rb
+++ getRSS/nokoFeeds.rb
@@ -9,15 +9,19 @@ require 'action_controller'
 
 conn = PGconn.open("dbname=d5do5eebiebbrt host=ec2-107-22-169-108.compute-1.amazonaws.com user=irgffdctcevjds password=akxBZ5CEfIbGgytAF2L9YfQo_E port=5432 sslmode=require")
 
+rfeed = Struct.new(:url, :name)
+
+
+
 #standard feeds
 rss_content = ""
-rss_ydn = "http://yaledailynews.com/feed/"
-rss_yherald = "http://yaleherald.com/feed/"
-rss_yalenews = "http://news.yale.edu/news-rss"
-rss_yathletics = "http://www.yalebulldogs.com/landing/headlines-featured?feed=rss_2.0"
-rss_ycc = "http://ycc.yale.edu/feed"
+rss_ydn = rfeed.new("http://yaledailynews.com/feed/", "Yale Daily News")
+rss_yherald = rfeed.new("http://yaleherald.com/feed/", "Yale Herald")
+rss_yalenews = rfeed.new("http://news.yale.edu/news-rss", "YaleNews")
+rss_yathletics = rfeed.new("http://www.yalebulldogs.com/landing/headlines-featured?feed=rss_2.0", "Yale Athletics")
+rss_ycc = rfeed.new("http://ycc.yale.edu/feed", "Yale College Council")
 
-rss_yrecord = "http://www.yalerecord.com/feed/"
+rss_yrecord = rfeed.new("http://www.yalerecord.com/feed/", "Yale Record")
 
 #non-yale general news feeds
 rss_nhregister = "http://www.nhregister.com/?rss=news"
@@ -27,35 +31,45 @@ rss_bbc = "http://feeds.bbci.co.uk/news/rss.xml"
 
 #yale publications (not news)
 rss_broadrecognition = "http://www.broadrecognition.com/feed" #updated feb 18
-rss_lightandtruth = "http://lightandtruthonline.wordpress.com/feed/" #updated dec 2012
-rss_logos = "http://yalelogos.com/feed/" #updated feb 15
-rss_newjournalatyale = "http://www.thenewjournalatyale.com/feed/" #updated dec 2012
-rss_thepolitic = "http://thepolitic.org/feed/" #updated feb 21
 rss_qmagazine = "http://www.qmagazineatyale.com/feed/" #updated feb 14
-rss_econreview = "http://yaleeconomicreview.com/feed/" #updated feb 20
-rss_epicurean = "http://yaleepicurean.com/feed/" #updated feb 18
-rss_globalist = "http://tyglobalist.org/feed/" #updated feb 21
-rss_jpublichealth = "http://theyaleph.com/feed" #updated feb 20
-rss_jmedlaw = "http://www.yalemedlaw.org/feed/" #updated feb 22
-rss_yscientific = "http://www.yalescientific.org/feed/" #updated feb 21
-rss_yuglawreview = "http://yulr.org/feed/" #updated feb 1
+
+rss_crosscampus = "http://yaledailynews.com/crosscampus/feed/"
+
+rss_lightandtruth = rfeed.new("http://lightandtruthonline.wordpress.com/feed/","Light and Truth Online") #updated dec 2012
+rss_logos = rfeed.new("http://yalelogos.com/feed/","Yale Logos") #updated feb 15
+rss_newjournalatyale = rfeed.new("http://www.thenewjournalatyale.com/feed/", "The New Journal at Yale") #updated dec 2012
+rss_thepolitic = rfeed.new("http://thepolitic.org/feed/","The Politic") #updated feb 21
+rss_econreview = rfeed.new("http://yaleeconomicreview.com/feed/", "Yale Economic Review") #updated feb 20
+rss_epicurean = rfeed.new("http://yaleepicurean.com/feed/","Yale Epicurean") #updated feb 18
+rss_globalist = rfeed.new("http://tyglobalist.org/feed/","The Globalist") #updated feb 21
+rss_jpublichealth = rfeed.new("http://theyaleph.com/feed", "The Yale Journal of PH") #updated feb 20
+rss_jmedlaw = rfeed.new("http://www.yalemedlaw.org/feed/", "Yale Journal of Medicine and Law") #updated feb 22
+rss_yscientific = rfeed.new("http://www.yalescientific.org/feed/","Yale Scientific") #updated feb 21
+rss_yuglawreview = rfeed.new("http://yulr.org/feed/", "Yale UG Law Review") #updated feb 1
+
+rss_rumpus = rfeed.new("http://www.yalerumpus.com/feed/", "Yale Rumpus")
+
+
+rss_yalefml = rfeed.new("http://yalefml.tumblr.com/rss", "YaleFML")
+
 
 #notes: logos doesn't work (atom in link)
 
-#rss_feeds = [rss_feed1, rss_feed2]
+
 rss_feeds = [rss_ydn, rss_yherald, rss_yalenews, rss_ycc]			#"http://news.yale.edu/news-rss"]
 
-#rss_feeds = [rss_broadrecognition, rss_lightandtruth, rss_newjournalatyale, rss_thepolitic, rss_qmagazine, rss_econreview, rss_epicurean, rss_globalist, rss_jpublichealth, rss_jmedlaw, rss_yscientific, rss_yuglawreview]
+#rss_feeds = [rss_lightandtruth, rss_newjournalatyale, rss_thepolitic, rss_econreview, rss_epicurean, rss_globalist, rss_jpublichealth, rss_jmedlaw, rss_yscientific, rss_yuglawreview, rss_rumpus]
+#note: don't use broad recognition or qmagazine
 
 #dbasetable = "otherpublications"
 dbasetable = "rss_articles2"
 #dbasetable = "rss_articles"
 
 rss_feeds.each do |rss_feed|
-doc = Nokogiri::XML(open(rss_feed)) #do |config|
+doc = Nokogiri::XML(open(rss_feed["url"])) #do |config|
 	#config.options = Nokogiri::XML::ParseOptions.STRICT
 
-source = doc.xpath("//rss/channel/title").first.text.gsub("'", "\\\\'")
+source = rss_feed["name"]		#doc.xpath("//rss/channel/title").first.text.gsub("'", "\\\\'")
 puts ''
 puts source
 
@@ -63,6 +77,8 @@ doc.remove_namespaces!
 
 #puts doc
 search = doc.css('item')
+
+prng = Random.new()
 
 if !search.empty?
 		search.each do |data|
@@ -82,9 +98,16 @@ if !search.empty?
 				content=''
 			end
 		#puts "#{title}"
-			query = "INSERT INTO #{dbasetable}(title, description, link, source, pubDate, content) VALUES (\'#{title}\',\'#{description}\',\'#{link}\',\'#{source}\',\'#{pubDate}\',\'#{content}\');"
-			res = conn.exec(query)
+			randid = prng.rand.to_s
+
+			doesexist = conn.exec("select id from #{dbasetable}  where link=\'#{link}\'");
+			ncopies = doesexist.ntuples();
+			
+			if (ncopies == 0)
+				query = "INSERT INTO #{dbasetable}(title, description, link, source, pubDate, content,randid) VALUES (\'#{title}\',\'#{description}\',\'#{link}\',\'#{source}\',\'#{pubDate}\',\'#{content}\', #{randid});"
+				res = conn.exec(query)
 				#puts "title: #{ title } link: #{ link }"
+			end
 		end
 end
 
