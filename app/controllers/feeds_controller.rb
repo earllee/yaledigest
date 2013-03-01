c2267ca8bbd21d3eefdd3eb08c679c370e53e672
earllee <earlvlee@gmail.com>
2013-03-01 00:43:34 -0500

    increased db query

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -5,7 +5,7 @@ require 'pg'
 class FeedsController < ApplicationController
 	def home
 		conn = PGconn.open("dbname=d5do5eebiebbrt host=ec2-107-22-169-108.compute-1.amazonaws.com user=irgffdctcevjds password=akxBZ5CEfIbGgytAF2L9YfQo_E port=5432 sslmode=require")
-		res = conn.exec('select * from (select * from rss_articles2 as orig order by pubDate desc limit 45) as neww order by randid asc;')
+		res = conn.exec('select * from (select * from rss_articles2 as orig order by pubDate desc limit 100) as neww order by randid asc;')
 		#	res = conn.exec('SELECT * FROM rss_articles')
 		@titles = res.column_values(0)
 		@articles = res
@@ -13,7 +13,7 @@ class FeedsController < ApplicationController
 
   def discover
 		conn = PGconn.open("dbname=d5do5eebiebbrt host=ec2-107-22-169-108.compute-1.amazonaws.com user=irgffdctcevjds password=akxBZ5CEfIbGgytAF2L9YfQo_E port=5432 sslmode=require")
-		res = conn.exec('select * from (select * from otherpublications as orig order by pubDate desc limit 45) as neww order by randid asc;')
+		res = conn.exec('select * from (select * from otherpublications as orig order by pubDate desc limit 100) as neww order by randid asc;')
 		#	res = conn.exec('SELECT * FROM rss_articles')
 		@titles = res.column_values(0)
 		@articles = res
